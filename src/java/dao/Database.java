392ed55ca37d5a7f98e832a4cc90b26a3a9cf30d
Mert Akel <akelmert95@gmail.com>
2018-05-12 17:42:28 +0300

    Update Database.java

--- src/java/dao/Database.java
+++ src/java/dao/Database.java
@@ -8,11 +8,11 @@ public class Database {
 	
 	public static Connection getConnection(){
 		Connection connection = null;
-		String connectionURL = "jdbc:oracle:thin:@10.6.10.5:1521:FONET";
+		String connectionURL = "jdbc:oracle:thin:@localhost:DbName";
 		try {
 			
 			Class.forName("oracle.jdbc.driver.OracleDriver").newInstance();
-			connection = DriverManager.getConnection(connectionURL, "STAJYER4", "Fonet5207");
+			connection = DriverManager.getConnection(connectionURL, "user", "password");
 
 		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
 			// TODO Auto-generated catch block
